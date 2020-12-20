a8ac959978e9a389355b00b6ece7c226e3d786e5
nicolas.jeanmair <nicolas.jean-mairet@he-arc.ch>
2020-12-20 22:38:16 +0100

    structure tache ok pour couleur de fond

--- lib/modeles/tache.dart
+++ lib/modeles/tache.dart
@@ -2,16 +2,17 @@ class Tache {
   final int id;
   final String titre;
   final String description;
+  final int couleurFond;
 
-  Tache({this.id,this.titre,this.description});
-
+  Tache({this.id, this.titre, this.description, this.couleurFond});
 
   //méthode qui va nous aider à convertir les dataObject en Map
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
       'titre': titre,
       'description': description,
+      'couleurFond': couleurFond,
     };
   }
 }
