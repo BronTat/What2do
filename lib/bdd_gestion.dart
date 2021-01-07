7537981562742c99fe7fd727fe1373e36f765123
nicolas.jeanmair <nicolas.jean-mairet@he-arc.ch>
2021-01-07 17:54:53 +0100

    config du systeme de notif continuation

--- lib/bdd_gestion.dart
+++ lib/bdd_gestion.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'main.dart';
 import 'modeles/tache.dart';
 import 'modeles/todo.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 class BDDGestion {
   Future<Database> bdd() async {
@@ -97,5 +100,50 @@ class BDDGestion {
     Database db = await bdd();
     await db.rawDelete("DELETE FROM taches WHERE id = '$id'");
     await db.rawDelete("DELETE FROM todo WHERE tacheId = '$id'");
+  }
+
+
+}
+
+//creer toutes les notifs.. les notifs restent si l'app est en tache de fond...
+//ce n'est pas des notifs push.. quand on ferme l'appli, elles disparaissent.
+void _createAllNotificationsFromTodos() async {
+  BDDGestion db =  BDDGestion();
+  //Init NotificationsDetails for android Platform
+  const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      "0", //id demandé
+      "taches", // nom du chanel de notif
+      "", //description du channel
+      importance: Importance.defaultImportance,
+      priority: Priority.defaultPriority,
+      showWhen: true); // quel moment a été déclenché la notif
+
+  //contient les settings pour android
+  //mcOs et iOS sont pas pris en compte car il faudrait tout gérer différement
+  const NotificationDetails platformChannelsSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+
+  List<Tache> taches = await db.getTaches();
+  for (int i = 0; i < taches.length; i++) {
+    List<Todo> todosTache = await db.getTodos(taches[i].id);
+    for (int j = 0; j < todosTache.length; j++) {
+      //print(todosTache[j].echeance);
+      if (todosTache[j].echeance != null) {
+        try {
+          await localNotifications.zonedSchedule( //creer la notif par rapport a une heure donnée
+              todosTache[j].id,
+              "La tâche " + todosTache[j].titre + " arrive à écheance",
+              "Dans 10 minutes, cette tâche aura passé son écheance",
+              tz.TZDateTime.parse(tz.local, todosTache[j].echeance).subtract(
+                  Duration(minutes: 10)),
+              platformChannelsSpecifics,
+              uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation
+                  .absoluteTime,
+              androidAllowWhileIdle: true, //meme si le telephone fait rien, la notif peut se lancer
+              payload: taches[i].id.toString());// info pour le selectNotification dans main.dart
+        } catch (ex) {
+          //debugPrint("ERROR");
+        }
+      }
+    }
   }
 }
