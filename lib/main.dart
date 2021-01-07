52ef28381b11a4cb24c5c46d6cee2a505cb3275a
nicolas.jeanmair <nicolas.jean-mairet@he-arc.ch>
2021-01-07 17:19:01 +0100

    initialisation du système de notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:todolist_app/screens/accueil.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
-void main() {
+FlutterLocalNotificationsPlugin localNotifications;
+final BehaviorSubject<String> selectNotificationSubject = BehaviorSubject<String>();
+
+void main() async {
+  //Obligatoire si initialisation dans "main" - Plugin flutter_local_notifications
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Configure la TimeZone (obligatoire pour les notifications prévus
+  tz.initializeTimeZones();
+
+  //Initialisation du plugin pour les notifications locales
+  localNotifications = FlutterLocalNotificationsPlugin();
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('todo_icon');
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid
+  );
+  await localNotifications.initialize(initializationSettings, onSelectNotification: selectNotification);
   runApp(MyApp());
+}
+
+//Fonction lancé si la notification est cliquer par l'utilisateur
+Future selectNotification(String payload) async {
+  if (payload != null) {
+    debugPrint('notification payload: $payload');
+  }
+  selectNotificationSubject.add(payload);
 }
 
 class MyApp extends StatelessWidget {
